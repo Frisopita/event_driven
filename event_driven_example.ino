728ad74af8a8716c838f151a241de0e3a6de96c7
Frida Sofía Andrade Sierra <frisoandrade@gmail.com>
2023-09-22 11:26:33 -0600

    se agrega switch case

--- event_driven_example.ino
+++ event_driven_example.ino
@@ -1,45 +1,55 @@
-//Test de event driven 
+//Test de event driven
 //Frida Sofia Andrade Sierra
 struct Button {
-    const uint8_t PIN;
-    uint32_t numberKeyPresses;
-    bool pressed;
+  const uint8_t PIN;
+  uint32_t numberKeyPresses;
+  bool pressed;
 };
 
 Button buttons[] = {
-    {15, 0, false},
-    {18, 0, false}
+  { 15, 0, false },
+  { 18, 0, false }
 };
 
-unsigned long button_time = 0;  
-unsigned long last_button_time = 0; 
+unsigned long button_time = 0;
+unsigned long last_button_time = 0;
 
 void IRAM_ATTR isr() {
-    button_time = millis();
+  button_time = millis();
 
-    for (int i = 0; i < sizeof(buttons) / sizeof(buttons[0]); i++) {
-        if (button_time - last_button_time > 250 && digitalRead(buttons[i].PIN) == LOW) {
-            buttons[i].numberKeyPresses++;
-            buttons[i].pressed = true;
-            last_button_time = button_time;
-        }
+  for (int i = 0; i < sizeof(buttons) / sizeof(buttons[0]); i++) {
+    if (button_time - last_button_time > 250 && digitalRead(buttons[i].PIN) == LOW) {
+      buttons[i].numberKeyPresses++;
+      buttons[i].pressed = true;
+      last_button_time = button_time;
     }
+  }
 }
 
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    for (int i = 0; i < sizeof(buttons) / sizeof(buttons[0]); i++) {
-        pinMode(buttons[i].PIN, INPUT_PULLUP);
-        attachInterrupt(digitalPinToInterrupt(buttons[i].PIN), isr, FALLING);
-    }
+  for (int i = 0; i < sizeof(buttons) / sizeof(buttons[0]); i++) {
+    pinMode(buttons[i].PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(buttons[i].PIN), isr, FALLING);
+  }
 }
 
 void loop() {
-    for (int i = 0; i < sizeof(buttons) / sizeof(buttons[0]); i++) {
-        if (buttons[i].pressed) {
-            Serial.printf("Button %d has been pressed %u times\n", i + 1, buttons[i].numberKeyPresses);
-            buttons[i].pressed = false;
-        }
+  for (int i = 0; i < sizeof(buttons) / sizeof(buttons[0]); i++) {
+    if (buttons[i].pressed) {
+      Serial.printf("Button %d has been pressed %u times\n", i + 1, buttons[i].numberKeyPresses);
+      buttons[i].pressed = false;
+      switch (i) {
+        case 0:
+          Serial.println("Leer sensor 1");
+          break;
+        case 1:
+          Serial.println("Leer sensor 2");
+          break;
+        default:
+          Serial.println("None");
+      }
     }
+  }
 }
